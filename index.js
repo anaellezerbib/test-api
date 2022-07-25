3da7f6a9cf37aa4dc83e7df1954af1e96125f5fa
Anaelle Zerbib <anaelle.zerbib@gmail.com>
2022-07-25 10:29:10 +0300

    api upload

--- index.js
+++ index.js
@@ -1,81 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+// const port = 5000;
 
-//let bodyParser = require('body-parser');
-//app.use(bodyParser.json());
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-const products = [
-{
-  id: 1,
-  name: "Ivanhoe",
-  author: "Sir Walter Scott",
-},
-{
-  id: 2,
-  name: "Colour Magic",
-  author: "Terry Pratchett",
-},
-{
-  id: 3,
-  name: "The Bluest eye",
-  author: "Toni Morrison",
-},
-{
-  id: 4,
-  name: "Belle du Seigneur",
-  author: "Albert Cohen",
-},
-{
-  id: 5,
-  name: "1984",
-  author: "George Orwell",
-},
-{
-  id: 6,
-  name: "Harry Potter and the Philosopher's stone",
-  author: "J. K. Rowling",
-},
-];
-
-app.post('/products', function(req, res) {
-  const newProduct = { ...req.body, id: products.length + 1 }
-  products = [ ...products, newProduct]
-  res.json(newProduct);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.put('/products', function(req, res) {
-  let updatedProduct;
-  products = products.map(p => {
-    if (p.id === req.body.id) {
-      updatedProduct = { ...p, ...req.body };
-      return updatedProduct;
-    }
-    return p;
-  })
-  res.json(updatedProduct);
+app.get("/products", (req,res) => {
+  const products = [
+  {
+    id: 1,
+    name: "hammer",
+  },
+  {
+    id: 2,
+    name: "screwdriver",
+  },
+  {
+    id: 3,
+    name: "wrench",
+  },
+ ];
+
+ res.json(products);
 });
 
-app.delete('/products/:id', function(req, res) {
-  const deletedProduct = products.find(p => p.id === +req.params.id);
-  products = products.filter(p => p.id !== +req.params.id);
-  res.json(deletedProduct);
+
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server up and listening`);
 });
-
-app.get('/products', (req, res) => {
-  const page = +req.query.page;
-  const pageSize = +req.query.pageSize;
-
-  if (page && pageSize) {
-    const start = (page - 1) * pageSize;
-    const end = start + pageSize;
-    res.json(products.slice(start, end));
-  } else {
-    res.json(products);
-  }
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-  

d8ae1189ffea5844e9b9e5e1fd67ea448099988b
Anaelle Zerbib <anaelle.zerbib@gmail.com>
2022-07-25 10:22:33 +0300

    api upload

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-let bodyParser = require('body-parser');
-app.use(bodyParser.json());
+//let bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 const products = [
 {
